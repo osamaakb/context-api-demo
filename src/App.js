29b07afe409c227bcce8febbab8b6b0a82d3eb85
osamaakb <osama98akb@gmail.com>
2021-01-16 13:21:38 +0300

    added button to one.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Loading from './components/Loading';
 import { StateContext } from "./State";
 
 export default function App() {
-  const [state, dispatch] = useContext(StateContext);
+  const [state] = useContext(StateContext);
 
   return (
-
     <div className="App" style={{ height: '100vh' }}>
       {state.isLoading && <Loading />}
       <One />
--- src/components/One.js
+++ src/components/One.js
@@ -12,9 +12,17 @@ const One = () => {
         dispatch({ type: 'CHANGE_THEME', payload: !state.dark })
     }
 
+    const loadTimer = () => {
+        dispatch({ type: 'SET_LOADING', payload: true })
+        setTimeout(() => {
+            dispatch({ type: 'SET_LOADING', payload: false })
+        }, 2000)
+    }
+
     return (
         <div className='border'>
             <button className={'button ' + theme} onClick={toggleHandler}>Change Theme in One</button>
+            <button className={'button ' + theme} onClick={loadTimer}>show loading for 2 seconds</button>
         </div>
     );
 };
